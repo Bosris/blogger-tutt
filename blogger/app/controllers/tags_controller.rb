9954025a22c5f49e8aa79e333a1b47532237cac3
james <jamesallstar21@gmail.com>
2019-03-09 07:51:05 -0500

    aded authentication

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,5 +1,16 @@
 class TagsController < ApplicationController
+    before_action :require_login, only: [:destroy]
+    
     def show
-    @tag = Tag.find(params[:id])
+        @tag = Tag.find(params[:id])
+    end
+    def index
+        @tag = Tag.all
+    end
+    def destroy
+        @tag = Tag.destroy(params[:id])    
+
+        flash.notice = "Tag '#{@tag.name}' has been deleted!"
+        redirect_to tags_path(@tag)
     end
 end
